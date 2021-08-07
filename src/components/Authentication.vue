2716559800555e60633a16162a0165523b8e3d34
Đorđe Starčević <djordje.starcevic@united.cloud>
2021-08-07 13:00:10 +0200

    Adds - register functionality in Authentication.vue

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -16,12 +16,12 @@
             @submit.prevent="
               mode === AuthMode.SignIn
                 ? signInWithEmailAndPassword(email, password)
-                : signUpWithEmailAndPassword(name, email, password)
+                : signUpWithEmailAndPassword(username, email, password)
             "
           >
             <ion-item v-if="mode === AuthMode.SignUp">
-              <ion-label position="floating"> Name </ion-label>
-              <ion-input v-model="name"></ion-input>
+              <ion-label position="floating"> Username </ion-label>
+              <ion-input v-model="username"></ion-input>
             </ion-item>
             <ion-item>
               <ion-label position="floating"> Email </ion-label>
@@ -55,6 +55,9 @@
         <ion-card-content v-if="errorMsg" class="error-message">
           {{ errorMsg }}
         </ion-card-content>
+        <ion-card-content v-if="successMsg" class="success-message">
+          {{ successMsg }}
+        </ion-card-content>
       </ion-card>
     </ion-content>
   </ion-page>
@@ -86,6 +89,14 @@ const AuthMode = {
   SignUp: "SignUp",
 };
 
+const defaulStateObj = {
+  username: "",
+  email: "",
+  password: "",
+  mode: AuthMode.SignIn,
+  successMsg: "",
+  errorMsg: "",
+};
 export default {
   name: "Authentication",
   components: {
@@ -104,15 +115,10 @@ export default {
     IonLabel,
     IonItem,
   },
+
   setup() {
     const router = useRouter();
-    const state = reactive({
-      name: "",
-      email: "",
-      password: "",
-      mode: AuthMode.SignIn,
-      errorMsg: "",
-    });
+    const state = reactive(defaulStateObj);
 
     const signInWithEmailAndPassword = (email, password) => {
       try {
@@ -120,11 +126,6 @@ export default {
           state.errorMsg = "Email and password required";
           return;
         }
-
-        // if(email !== 'djole@djole' && password !== 'djole') {
-        //   state.errorMsg = "No user with that email and password"
-        //   return
-        // }
 
         var payload = {
           email: email,
@@ -144,7 +145,7 @@ export default {
             router.push({ path: "/tabs/tab1" });
           })
           .catch(() => {
-            state.errorMsg = "No user with such email and password."
+            state.errorMsg = "No user with such email and password.";
           });
         // await auth.signInWithEmailAndPassword(email, password) ???
       } catch (error) {
@@ -152,21 +153,41 @@ export default {
       }
     };
 
-    const signUpWithEmailAndPassword = (name, email, password) => {
+    const signUpWithEmailAndPassword = (username, email, password) => {
       try {
-        if (!name || !email || !password) {
-          state.errorMsg = "Name, email and password required";
+        if (!username || !email || !password) {
+          state.errorMsg = "Username, email and password required";
           return;
         }
 
+        var payload = {
+          username: username,
+          email: email,
+          password: password,
+        };
+
+        fetch("http://localhost:8080/carpool-be/api/user/registration", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        })
+          .then((response) => {
+            console.log(response);
+            state.email = ''
+            state.password = ''
+            state.username = ''
+            state.mode = AuthMode.SignIn
+            state.successMsg = "Succesfull register. Please activate your email."
+          })
+          .catch((error) => console.log(error));
         // const authRes = await auth.createUserWithEmailAndPassword(email, password)
 
         // db.collection('users').doc(authRes.user?.uid).set({
-        //     name,
+        //     Username,
         //     email
         // })
-
-        router.push({ path: "/tabs/tab1" });
       } catch (error) {
         state.errorMsg = error.message;
       }
@@ -199,6 +220,12 @@ export default {
   color: #842029;
   background-color: #f8d7da;
   border-color: #f5c2c7;
+  text-align: center;
+}
+
+.success-message {
+  color: #6f8d21;
+  background-color: #dff2bf;
   text-align: center;
 }
 </style>
