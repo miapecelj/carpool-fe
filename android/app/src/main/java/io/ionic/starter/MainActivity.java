16a7684ab628e228e95d1f45d48d3213cc7bb782
emapiric <emilija.piric@dericon.de>
2021-09-05 14:06:47 +0200

    notifications init

---
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,6 @@
+package io.ionic.starter;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.fcm.FCMPlugin;
+
+public class MainActivity extends BridgeActivity {}
--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,54 @@
 <script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { FCM } from '@capacitor-community/fcm';
+import { Plugins } from '@capacitor/core';
+const { PushNotifications } = Plugins;
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
     IonRouterOutlet
+  },
+  data() {
+    return {
+      fcm: new FCM()
+    }
+ },
+  mounted() {
+    
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+PushNotifications.requestPermission().then(result => {
+      alert("result " + JSON.stringify(result));
+    });
+         // Add registration error if there are.
+        PushNotifications.addListener("registrationError", (error) => {
+          console.log(`error on register ${JSON.stringify(error)}`);
+        }),
+        // Add Notification received
+          PushNotifications.addListener(
+            "pushNotificationReceived",
+            (notification) => {
+              console.log(`notification ${JSON.stringify(notification)}`);
+            }
+          ),
+          // Add Action performed
+          PushNotifications.addListener(
+            "pushNotificationActionPerformed",
+            async (notification) => {
+                alert("notification " + notification)
+              console.log("notification succeeded");
+            }
+          ),
+          // Initialize the registration with FCM Token
+          PushNotifications.register();
+        const fcmToken = this.fcm.getToken();
+       alert(JSON.stringify(fcmToken));
+        console.log("token:" + JSON.stringify(fcmToken));
   }
+
 });
 </script>
